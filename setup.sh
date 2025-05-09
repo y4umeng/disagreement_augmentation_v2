bd23c1f76e580600d4b1e93d7db2397e0df25b01
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2025-05-09 17:17:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ source .venv/bin/activate
 pip install -r requirements.txt
 python3 setup.py develop
 pip install -e .
+
+mkdir download_ckpts
+cd download_ckpts
+wget https://github.com/megvii-research/mdistiller/releases/download/checkpoints/cifar_teachers.tar
+tar -xvf cifar_teachers.tar
