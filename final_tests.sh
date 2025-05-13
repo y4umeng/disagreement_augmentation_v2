6c1053e721fc2eaf1bf90fb65254e7ebcfc1d040
root <yw3809@columbia.edu>
2025-05-13 03:33:03 +0000

    final tests

---
+++ final_tests.sh
@@ -0,0 +1,7 @@
+python tools/train.py --cfg configs/cifar100/da_mse/ResNet50_MobileNetV2.yaml
+python tools/train.py --cfg configs/cifar100/da_mse/ResNet50_MobileNetV2.yaml
+python tools/train.py --cfg configs/cifar100/da_mse/ResNet50_MobileNetV2.yaml
+python tools/train.py --cfg configs/cifar100/da_mse/ResNet50_MobileNetV2.yaml
+python tools/train.py --cfg configs/cifar100/da_mse/ResNet50_MobileNetV2.yaml
+
+python tools/train.py --cfg configs/cifar100/da_mse/resnet32x4_wrn_40_2.yaml
